66fcb4f85cf64c92530579de788a82a81ade4e59
Akinbamido Sunday Yemi <africpoet@gmail.com>
2024-04-03 11:52:34 +0100

    Day 2: Project Folder Structure, Templates and Other Resources

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
+import os
+APP_NAME = os.getenv('APP_NAME')
+author = os.getenv('AUTHOR')
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "RBAC: Web Application Development, Using MVC Architecture."
+@app.route('/login')
+def login():
+    return render_template('auth/signin.html', title='Sign In', app_name=APP_NAME, author=author)
+
+@app.route('/register')
+def register():
+    return render_template('auth/signup.html', title='Sign Up', app_name=APP_NAME, author=author)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('portal/dashboard.html', title="Dashboard", app_name=APP_NAME, author=author)
+
